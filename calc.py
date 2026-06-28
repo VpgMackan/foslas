ac0a71798101bcfafe44517934fce7af09fec23f
Markus Nielsen <52716811+VpgMackan@users.noreply.github.com>
2026-06-28 08:55:17 +0200

    Moved to a class

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-BIG_G = 6.67 * 10 ** (-11)
-BIG_M = 1.989 * 10**30
+SUN_GRAVITATIONAL_CONSTANT = 6.67e-11  # m^3 kg^-1 s^-2
+SUN_MASS = 1.989e30
 
 
 # TODO (fuck)
@@ -12,103 +12,82 @@ def calc_kep_3(semi_major_axis=None, orbital_period=None) -> float:
         return np.sqrt(semi_major_axis**3)
 
 
-def calc_vis_viva(current_radius: float, semi_major_axis: float) -> float:
-    return np.sqrt(BIG_G * BIG_M * (2 / current_radius - 1 / semi_major_axis))
+class OrbitalStructure:
+    def __init__(
+        self,
+        semi_major_axis: float,
+        current_radius: float,
+        apogee: float,
+        perigee: float,
+    ):
+        self.semi_major_axis = semi_major_axis
+        self.current_radius = current_radius
+        self.apogee = apogee
+        self.perigee = perigee
 
+        if perigee <= 0 or apogee < perigee:
+            raise ValueError("Ogiltiga banparametrar: apogee >= perigee > 0 krävs")
+        else:
+            self.eccentricity = (apogee - perigee) / (apogee + perigee)
 
-def calc_cen_acceleration(current_radius: float) -> float:
-    return (BIG_G * BIG_M) / (current_radius**2)
+        self.semi_minor_axis = np.sqrt(
+            (self.semi_major_axis**2) / (self.eccentricity - 1)
+        )
+        self.vis_viva_velocity = np.sqrt(
+            SUN_GRAVITATIONAL_CONSTANT
+            * SUN_MASS
+            * (2 / self.current_radius - 1 / self.semi_major_axis)
+        )
+        self.acceleration = (SUN_GRAVITATIONAL_CONSTANT * SUN_MASS) / (
+            self.current_radius**2
+        )
 
+        self.area = 2 * np.pi * self.semi_minor_axis * self.semi_major_axis
 
-def calc_eccentricity(apogee: float, perigee: float) -> float:
-    """
-    Beräknar excentriciteten för en elliptisk omloppsbana.
+        if self.perigee <= 0:
+            raise ValueError("perigee måste vara > 0")
+        elif self.apogee < self.perigee:
+            raise ValueError("apogee måste vara >= perigee")
+        else:
+            self.focal_distance = self.apogee - self.perigee
 
-    Args:
-        apogee:  Avstånd vid apogeepunkten (från kroppens centrum)
-        perigee: Avstånd vid perigeepunkten (från kroppens centrum)
+    def calc_angle(self, target_orbit: float) -> float:
+        """
+        Computes the satellite's perpendicular height above the major axis,
+        using the law of cosines on the triangle formed by the two foci
+        and the satellite's current position on the ellipse.
 
-    Returns:
-        Excentricitet e (0 = cirkel, 0 < e < 1 = ellips)
-    """
-    if perigee <= 0 or apogee < perigee:
-        raise ValueError("Ogiltiga banparametrar: apogee >= perigee > 0 krävs")
+        Args:
+            target_orbit: Orbital radius from the central body [km]
 
-    return (apogee - perigee) / (apogee + perigee)
+        Returns:
+            Perpendicular distance from the major axis [km]
+        """
+        major_axis = self.apogee + self.perigee  # 2a
 
+        # Law of cosines: resolve angle θ at the occupied focus
+        cos_theta = (target_orbit**2 + self.focal_distance**2 - major_axis**2) / (
+            2 * target_orbit * self.focal_distance
+        )
 
-def calc_semi_minor_axis(eccentricity: float, semi_major_axis: float) -> float:
-    return np.sqrt((semi_major_axis**2) / (eccentricity - 1))
+        return np.arccos(cos_theta)
 
+    def calc_area_sector(self, cos_theta: float, r: float) -> float:
+        return (
+            np.arctan(r * np.tan(cos_theta) / self.focal_distance)
+            * r
+            * self.focal_distance
+            * 1
+            / 2
+        )
 
-def calc_area(semi_minor_axis: float, semi_major_axis: float) -> float:
-    """
-    Calc area, not that complicated bro. You can figure it out yourself
-    """
-    return 2 * np.pi * semi_minor_axis * semi_major_axis
+    def calc_delta_orbital_period(
+        self, orbital_period: float, area_sector: float, area: float
+    ):
+        return (orbital_period * area_sector) / area
 
-
-def calc_ref_point(apogee: float, perigee: float) -> float:
-    """
-    I don't know bro, I don't ork
-
-    Args:
-        fuck if i know
-
-    Returns:
-        something sometimes
-    """
-    if perigee <= 0:
-        raise ValueError("perigee måste vara > 0")
-    if apogee < perigee:
-        raise ValueError("apogee måste vara >= perigee")
-
-    return apogee - perigee
-
-
-def calc_angle(target_orbit: float, apogee: float, perigee: float) -> float:
-    """
-    Computes the satellite's perpendicular height above the major axis,
-    using the law of cosines on the triangle formed by the two foci
-    and the satellite's current position on the ellipse.
-
-    Args:
-        target_orbit: Orbital radius from the central body [km]
-        apogee:       Apogee distance from the central body  [km]
-        perigee:      Perigee distance from the central body [km]
-
-    Returns:
-        Perpendicular distance from the major axis [km]
-    """
-    focal_distance = calc_ref_point(apogee, perigee)  # d = 2c
-    r = target_orbit
-    major_axis = apogee + perigee  # 2a
-
-    # Law of cosines: resolve angle θ at the occupied focus
-    cos_theta = (r**2 + focal_distance**2 - major_axis**2) / (2 * r * focal_distance)
-
-    return np.arccos(cos_theta)
-
-
-def calc_area_sector(
-    cos_theta: float, r: float, apogee: float, perigee: float
-) -> float:
-    focal_distance = calc_ref_point(apogee, perigee)
-    return (
-        np.arctan(r * np.tan(cos_theta) / focal_distance) * r * focal_distance * 1 / 2
-    )
-
-
-def calc_delta_orbital_period(orbital_period: float, area_sector: float, area: float):
-    return (orbital_period * area_sector) / area
-
-
-def calc_delta_theta_ship(orbital_period: float) -> float:
-    return 360 / (orbital_period * 365)
-
-
-def calc_delta_theta_body(orbital_period_body: float) -> float:
-    return 360 / (orbital_period_body * 365)
+    def calc_delta_theta(self, orbital_period: float, days=365) -> float:
+        return 360 / (orbital_period * days)
 
 
 if __name__ == "__main__":
